004bbde9e1957acb8f0253af09ad4c26052da929
Equilliam <eamonquill@gmail.com>
2026-04-22 00:31:47 -0300

    Main Arduino code file

---
+++ DalMAST_NRF24_Receiver.ino
@@ -0,0 +1,47 @@
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+Servo myServo;
+
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+  myServo.attach(5);
+}
+
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
+
+
+void motor() {
+  delay(5);
+  if ( radio.available()) {
+    while (radio.available()) {
+      int angleV = 0;
+      radio.read(&angleV, sizeof(angleV));
+      myServo.write(angleV);
+    }
+  }
+}
